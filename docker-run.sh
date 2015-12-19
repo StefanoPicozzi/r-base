2f4f2721d53af3d07c97528257ec8da1b83b3315
Stefano.Picozzi@gmail.com <StefanoPicozzi@gmail.com>
2015-12-20 07:50:46 +1030

    x

---
+++ docker-run.sh
@@ -0,0 +1,3 @@
+# docker run -ti --rm -v "$PWD":/home/docker -w /home/docker -u docker r-base R CMD check .
+docker run -ti --rm -v "$PWD":/home/docker -w /home/docker -u docker r-base R CMD BATCH $1
+# docker run -ti --rm -v "$PWD":/home/docker -w /home/docker -u docker r-base /bin/bash
